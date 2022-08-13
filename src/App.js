838725180e2353618f97094fb71238839bd6e838
Edwin Tumbaga <edwin.tumbaga@gmail.com>
2022-08-13 01:11:09 -0400

    Make radio path the default page upon load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import RadioButtons from "./pages/RadioButtons";
 import Unauthorized from "./pages/Unauthorized";
@@ -12,7 +12,7 @@ function App() {
     <Navbar />
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index element={<Navigate to="radio" />} />
           <Route path="radio" element={<RadioButtons />} />
           <Route path="*" element={<Unauthorized />} />
         </Route>
